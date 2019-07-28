00472f4a04dee827515bd3a914bed42467af26d7
Amy Fogelman <abfnj1@gmail.com>
2019-07-28 20:28:33 +0000

    Automatically backed up by Learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -22,3 +22,15 @@ class Genre
     end
   end
 end
+
+jay_z = Artist.new("Jay-Z")
+rap = Genre.new("rap")
+ninety_nine_problems = Song.new("99 Problems", rap)
+jay_z.add_song(ninety_nine_problems)
+another_crappy_song = Song.new("Crappy Song", rap)
+jay_z.add_song(another_crappy_song)
+jay_z.songs
+# ninety_nine_problems.genre
+rap.songs 
+
+rap.songs.collect {|i| i.name}
